cebb81b5f8d8f12c3dd0f3b1ce3f33ad557f2981
ooskdud <ooskdud@gmail.com>
2021-06-25 22:05:02 +0900

    메모 view추가

--- calender/forms.py
+++ calender/forms.py
@@ -1,9 +1,7 @@
 from django import forms
+from .models import Memo
 
 class Memo_createForm(forms.Form):
-    # group = forms.
-    title = forms.CharField(max_length=100)
-    body = forms.CharField(widget=forms.Textarea)
-
-    def save(self, *args, **kwargs):
-        super(Memo_createForm, self).save(*args,**kwargs)
+    class Meta:
+        model = Memo 
+        fields = ['title', 'body']
--- calender/views.py
+++ calender/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def home(request):
     return render(request,'home.html')
 
-# def memo_create(request):
-#     if request.method == 'POST':
-#         form = Memo_createForm(request.POST)
-#         if form.is_valid():
+def memo_create(request):
+    if request.method == 'POST':
+        form = Memo_createForm(request.POST)
+        if form.is_valid():
+            Memo = form.save(commit=False)
+        return redirect('/Memo_detail/'+str(Memo.id))
+    else:
+        form = Memo_createForm()
+    return render(request, 'memo_create.html', {'form':form})
             
