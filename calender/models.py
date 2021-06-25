979cc11355720f2368587d266149756baa117260
ooskdud <ooskdud@gmail.com>
2021-06-25 22:10:39 +0900

    메모장 추가

--- calender/models.py
+++ calender/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Memo(models.Model):
-    title = models.CharField(max_lengh=100)
+    title = models.CharField(max_length=100)
     body = models.TextField()
